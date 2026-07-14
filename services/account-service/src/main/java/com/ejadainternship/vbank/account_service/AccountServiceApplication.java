2f4a4984f768a6c9970b948a61224c23de6b8582
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-14 18:11:44 +0300

    feat: init repo

---
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ejadainternship.vbank.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
