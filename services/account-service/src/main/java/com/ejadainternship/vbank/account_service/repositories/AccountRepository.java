04685cd994a01d80fa98e17b56ba54f57cb691fc
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-15 20:28:46 +0300

    feat(account): add findAllByUserId to repo

--- services/account-service/src/main/java/com/ejadainternship/vbank/account_service/repositories/AccountRepository.java
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/repositories/AccountRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
+    public List<Account> findAllByUserId(String userId);
 }
