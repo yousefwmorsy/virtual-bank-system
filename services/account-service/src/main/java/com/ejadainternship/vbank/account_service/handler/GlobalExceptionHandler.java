96fc3089b030d3b23abce6c9c35d713b91ff0f59
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-15 20:31:03 +0300

    fix(account): add insufficient funds handling and fallback

--- services/account-service/src/main/java/com/ejadainternship/vbank/account_service/handler/GlobalExceptionHandler.java
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ejadainternship.vbank.account_service.handler;
 
 import com.ejadainternship.vbank.account_service.exceptions.AccountDoesNotExistException;
+import com.ejadainternship.vbank.account_service.exceptions.InsufficientBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AccountDoesNotExistException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(AccountDoesNotExistException e) {
+    public ResponseEntity<?> handleAccountDoesNotExistException(AccountDoesNotExistException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<?> handleInsufficientBalanceException(InsufficientBalanceException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
