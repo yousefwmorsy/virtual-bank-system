99522633cb53bf3ef69bcddcad215c67cc8c161b
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-15 20:28:14 +0300

    feat(account): add mapper to account details dto

---
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/mapper/AccountMapper.java
@@ -0,0 +1,16 @@
+package com.ejadainternship.vbank.account_service.mapper;
+
+import com.ejadainternship.vbank.account_service.dtos.AccountDetailsDTO;
+import com.ejadainternship.vbank.account_service.models.Account;
+
+public class AccountMapper {
+    public static AccountDetailsDTO toAccountDetailsDTO(Account account){
+        return new AccountDetailsDTO(
+                account.getId(),
+                account.getAccountNumber(),
+                account.getAccountType().toString(),
+                account.getBalance(),
+                account.getStatus().toString()
+        );
+    }
+}
