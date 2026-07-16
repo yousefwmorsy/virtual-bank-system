50f67c435c537afdc230bd2a6ed9ddf6e1581c7d
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-16 17:21:30 +0300

    docs(account): mark UserService as TODO for integration

--- services/account-service/src/main/java/com/ejadainternship/vbank/account_service/services/UserService.java
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/services/UserService.java
@@ -1,7 +1,8 @@
 package com.ejadainternship.vbank.account_service.services;
 
 public class UserService {
-    public boolean verifyUser(String username){
+    public boolean verifyUser(String userId){
+        // TODO: Implement gRPC/REST to verify user using user-service
         return true;
     }
 }
