79a13f0569f48b61cf8abc7cf339f537795bdeb5
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-16 11:02:06 +0300

    fix(account): correct CreateAccountRequestDTO initialBalance format

--- services/account-service/src/main/java/com/ejadainternship/vbank/account_service/dtos/CreateAccountRequestDTO.java
+++ services/account-service/src/main/java/com/ejadainternship/vbank/account_service/dtos/CreateAccountRequestDTO.java
@@ -1,8 +1,10 @@
 package com.ejadainternship.vbank.account_service.dtos;
 
+import java.math.BigDecimal;
+
 public record CreateAccountRequestDTO(
         String userId,
         String accountType,
-        String initialBalance
+        BigDecimal initialBalance
 ) {
 }
