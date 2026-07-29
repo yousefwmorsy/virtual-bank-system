9852bc00a6e8ef4e7f637a7e61b92f3eae31fe41
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-29 15:04:50 +0300

    feat(auth): add client type request header to generate jwt mapped to the wso2 application

--- services/user-service/src/main/java/com/example/userservice/services/AuthService.java
+++ services/user-service/src/main/java/com/example/userservice/services/AuthService.java
@@ -13,7 +13,6 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.MissingRequestHeaderException;
 
 import java.time.Instant;
 import java.util.List;
